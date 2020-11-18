021fe85b16501e3e5d5f981c1460228b781a7490
lizuncong <2900170896@qq.com>
2020-11-18 23:06:05 +0800

    stash

--- 框架原理/react/lib/react-dom.js
+++ 框架原理/react/lib/react-dom.js
@@ -1,5 +1,14 @@
-function render(vdom, container){
-  console.log('render...', vdom, container)
+function render(virtualDom, parent){
+  let dom = document.createElement(virtualDom.type);
+  Object.keys(virtualDom.props)
+      .filter(key => key !== 'children')
+      .forEach(key => {
+        dom[key] = virtualDom.props[key];
+      })
+  if(Array.isArray(virtualDom.props.children)){
+    virtualDom.props.children.forEach(child => render(child, dom))
+  }
+  parent.appendChild(dom)
 }
 
 export default { render }
--- 框架原理/react/source/src/index.jsx
+++ 框架原理/react/source/src/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from '../../lib/react-dom';
 
+let element = (
+  <div id="A">
+    A
+    <div id="B">
+      <div id="C">C</div>
+      <div id="D">D</div>
+    </div>
+    <div id="E">E</div>
+  </div>
+)
+
+console.log('element...', JSON.stringify(element, null, 2))
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <div>react app</div>,
+  element,
   rootElement,
 );
