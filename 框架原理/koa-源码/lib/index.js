3f3bf4925f24d89c862f709d7ba368ff8a8c9b8e
lizuncong <2900170896@qq.com>
2020-12-02 18:53:20 +0800

    stash

--- 框架原理/koa-源码/lib/index.js
+++ 框架原理/koa-源码/lib/index.js
@@ -5,6 +5,29 @@ const context = require('./context')
 
 class MiniKoa {
 
+    constructor(){
+        this.middlewares = [];
+    }
+
+    compose(middlewares) {
+        return function(ctx){
+            return dispatch(0);
+
+            function dispatch(i){
+                let mid = middlewares[i];
+                if(!mid){
+                    return Promise.resolve();
+                }
+                return Promise.resolve(
+                    mid(ctx, function next(){
+                        // promise完成后，再执行下一个
+                        return dispatch(i + 1)
+                    })
+                )
+            }
+        }
+    }
+
     createContext(req, res){
         // Object.create原型式继承
         const ctx = Object.create(context)
@@ -19,10 +42,15 @@ class MiniKoa {
 
     listen(...args){
         http
-            .createServer((req, res) => {
+            .createServer(async (req, res) => {
                 // 创建上下文对象
                 const ctx = this.createContext(req, res)
-                this.callback(ctx)
+
+                // 将middlewares复合成一个函数，fn是个异步
+                const fn = this.compose(this.middlewares)
+
+                await fn(ctx)
+                // this.callback(ctx)
 
                 // 给用户返回数据
                 res.end(ctx.body)
@@ -30,8 +58,8 @@ class MiniKoa {
             .listen(...args)
     }
 
-    use(callback){
-        this.callback = callback
+    use(mid){
+        this.middlewares.push(mid)
     }
 }
 
