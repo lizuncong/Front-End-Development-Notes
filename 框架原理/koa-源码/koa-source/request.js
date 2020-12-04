b052baf3693afcf4953acfd8c55d0da737676657
lizuncong <2900170896@qq.com>
2020-12-04 17:47:23 +0800

    stash

--- 框架原理/koa-源码/koa-source/request.js
+++ 框架原理/koa-源码/koa-source/request.js
@@ -1,26 +1,5 @@
 module.exports = {
-
-    get header() { // nodejs原生的headers对象
-        return this.req.headers;
-    },
-    set header(val) {
-        this.req.headers = val;
-    },
-
-    get url() { // nodejs原生的url
-        return this.req.url;
-    },
-
-    set url(val) {
-        this.req.url = val;
-    },
-
-    get method() {
-        return this.req.method;
-    },
-
-    set method(val) {
-        this.req.method = val;
-    },
-
-};
+    get url(){
+        return this.req.url
+    }
+}
--- 框架原理/koa-源码/src/简单的node服务.js
+++ 框架原理/koa-源码/src/简单的node服务.js
@@ -1,16 +1,16 @@
 const http = require('http')
-const parse = require('parseurl');
 
 const hostname = '127.0.0.1'
 
 const port = 5001
 
 const server = http.createServer((req, res) => {
-    console.log(res.statusCode, res.headerSent)
+    console.log(res.statusCode, res.headersSent)
     console.log(req.method)
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
+    console.log(res.statusCode, res.headersSent)
 })
 
 
